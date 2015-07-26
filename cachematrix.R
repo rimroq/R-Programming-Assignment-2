377858b0b988663d317f493df7cce1c5295bf46f
rimroq <rimroq@gmail.com>
2015-07-26 23:55:57 +0100

    Commit for Programming Assignment 2
    Exercise to Cache inverted matrices to avoid unnecessary
    re-calculations

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+##Exercise for Programming Assignment 2 in Programming in R for JHU Data Specialization Course July 2015
 
-makeCacheMatrix <- function(x = matrix()) {
+##The assignment is to to write a pair of functions that avoid unnecessarily repeating inversions of matrices by caching the inversions and retrieving the caches rather than re-computing the inverse
+## IF the matrix has changed since the last inversion, the inversion needs to be re-done. (but i'm not sure how to do this part...?)
 
+##The code below is based on the two examples for calculating and caching the mean provided in the assignment
+
+
+makeCacheMatrix <- function(myx = matrix()) {
+ myInverse <- NULL
+ set <- function(x) {
+        myx <<- x;
+        myInverse <<- NULL;
+    }
+    get <- function() (myx);
+    setinv <- function(solve) myInverse <<- solve;
+    getinv <- function() myInverse;
+    list(set = set, get = get, 
+          setinv = setinv, 
+          getinv = getinv)    
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+##cacheSolve <- function(x, ...) {
+        ## Return a matrix that is (hopefullly?)the inverse of 'x'
+##}
+
+
+## Compute the inverse of the
+## "matrix" returned by `makeCacheMatrix` above, provided the matrix has not changed, otherwise re-calc inverse
+## return the inverse...
+
+cacheSolve <- function(myx, ...) {
+    myInverse <- myx$getinv()
+    if(!is.null(myInverse)) {
+        message("Cache exists... now retrieving cached data...")
+        return(inverse)
+    }	
+    myData <- myx$get()
+    myInverse <- solve(myData, ...)
+    myx$setinv(myInverse)
+   myInverse
 }
